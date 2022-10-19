9082971d2aef0da1751e3d87b0ea59833690b5bb
seyren10 <gagarinroyvictor@gmail.com>
2022-10-19 21:07:47 +0800

    Updated C# Book
    -up to if else statement

--- Assets/_Scripts/UI/InfoDisplay.cs
+++ Assets/_Scripts/UI/InfoDisplay.cs
@@ -1,24 +1,34 @@
 using UnityEngine;
 using TMPro;
-
+using UnityEngine.UI;
 public class InfoDisplay : MonoBehaviour
 {
     private string[] textDataArray;
     private int dataIndex;
 
     private TMP_Text dataText;
+    private Button btnPrev;
+    private Button btnNext;
 
     private void Awake()
     {
         dataText = transform.Find("dataText").GetComponent<TMP_Text>();
+        btnPrev = transform.Find("btnPrev").GetComponent<Button>();
+        btnNext = transform.Find("btnNext").GetComponent<Button>();
     }
 
     public void SetTextDataArray(string[] textDataArray)
     {
+        btnNext.interactable = false;
+        btnPrev.interactable = false;
+
         dataIndex = 0;
         this.textDataArray = textDataArray;
 
         DisplayText();
+
+        if (textDataArray.Length > 1)
+            btnNext.interactable = true;
     }
 
     private void DisplayText()
@@ -31,7 +41,14 @@ public class InfoDisplay : MonoBehaviour
         if (textDataArray == null) return;
 
         if (dataIndex < textDataArray.Length - 1)
+        {
             dataIndex++;
+            btnPrev.interactable = true;
+        }
+
+        if (dataIndex >= textDataArray.Length - 1)
+            btnNext.interactable = false;
+
 
         DisplayText();
     }
@@ -40,7 +57,13 @@ public class InfoDisplay : MonoBehaviour
         if (textDataArray == null) return;
 
         if (dataIndex > 0)
+        {
             dataIndex--;
+            btnNext.interactable = true;
+        }
+
+        if (dataIndex <= 0)
+            btnPrev.interactable = false;
 
         DisplayText();
     }
